5dfc04d14054f2c6759a28e999c179e6c870bffe
JoaoP2006 <165199071+JoaoP2006@users.noreply.github.com>
2025-06-05 11:46:08 -0300

    Avaliaçao de PDS

---
+++ AvaliacaoPDS2/PDS2-I6-25-1/LojaMVC/src/main/java/controller/ClienteView.java
@@ -0,0 +1,23 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClienteView {
+
+    @FXML
+    private TextField txtDataNasc;
+
+    @FXML
+    private TextField txtEndereco;
+
+    @FXML
+    private TextArea txtListaClientes;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtTelefone;
+
+}
---
+++ AvaliacaoPDS2/PDS2-I6-25-1/LojaMVC/src/main/java/principal/App.java
@@ -0,0 +1,53 @@
+package principal;
+
+import controller.LoginController;
+import java.io.File;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import model.Cliente;
+import model.ClienteDAO;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("Loja MVC");
+        URL url = new File("src/main/java/view/Login.fxml").toURI().toURL();
+        FXMLLoader loader = new FXMLLoader(url);
+        Parent root = loader.load();
+        Stage telaLogin = new Stage();
+        LoginController lc = loader.getController();
+        lc.setStage(telaLogin);
+        telaLogin.setOnShown(event -> {
+            lc.abrirJanela();
+        });
+        Scene scene = new Scene (root);
+        scene.getStylesheets().add(getClass().getResource(""
+                + "/css/login.css").toExternalForm());
+        telaLogin.setScene(scene);
+        telaLogin.show();
+    }
+
+    public static void main(String[] args) {
+       ClienteDAO clienteDAO = new ClienteDAO();
+       
+       Cliente novoCliente = new Cliente();
+       novoCliente.setNome("Ana Maria");
+       novoCliente.setTelefone("11999999999");
+       novoCliente.setEndereco("rua das flores, 123");
+       novoCliente.setDataNascimento(Date.valueOf("1990-05-10"));
+       
+       clienteDAO.inserirCliente(novoCliente);
+       
+       clienteDAO.listarClientes();
+     }
+    
+
+}
